b00cf85d08dfc024959fe5068dca2cf08788f1d4
Sergey Korepanov <s.a.korepanov@mail.ru>
2020-05-27 14:17:57 +0400

    Initial commit

---
+++ src/main/java/com/scorescode/todolist/backendtodolist/BackendTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.scorescode.todolist.backendtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scorescode/todolist/backendtodolist/BackendTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorescode.todolist.backendtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
